e1dcad7e9ff1ac2b61054e9e5673d406d59a8848
Jertt <jeartt.th@gmail.com>
2022-07-04 23:26:42 +0200

    learning global objects first lesson

---
+++ global.js
@@ -0,0 +1,18 @@
+// Global Object
+
+// console.log(global);
+
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('jertt is going to be *i*h_***');
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
+
+// console.log(document.querySelector); doesn't work because we are not going to need node to communnicate with the window side only with the server side :)
+
--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const name = 'jerttinjo';
+const name = 'jertt is going to the moon with' + ' 5 milly cash';
 
 console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('mario');
+greet('yooshi');
