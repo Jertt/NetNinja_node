fdeddb8e635f526adea223fc22cbd576b8cc27b8
Jertt <jeartt.th@gmail.com>
2022-07-06 16:31:55 +0200

    reading streams, writing streams, and aslo piping which is the easiest waygit add .!

--- streams.js
+++ streams.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blogl.txt', {encoding: 'utf-8'});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-readStream.on('data', (chunk) => {
-    console.log('------ NEW CHUNK ------');
-    console.log(chunk);
-});
+// readStream.on('data', (chunk) => {
+//     console.log('------ NEW CHUNK ------');
+//     console.log(chunk);
+//     writeStream.write('\nNew Chunk\n');
+//     writeStream.write(chunk);
+// });
+
+// when we use a pipe it must be from a readable stream to a writeable one!
+
+// piping
+
+readStream.pipe(writeStream);
